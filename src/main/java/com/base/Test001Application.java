d104a06fc5d290488c0a4e41f545baeb55ca13ad
kyounghun park <pkh3116@aend.co.kr>
2020-09-18 17:10:27 +0900

    바나나 차차

--- src/main/java/com/base/Test001Application.java
+++ src/main/java/com/base/Test001Application.java
@@ -3,6 +3,19 @@ package com.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+* @packageName   : com.base
+* @fileName      : Test001Application.java
+* @author        : HUN
+* @date          : 2020.09.18
+* @description   : 바나나차차
+* ===========================================================
+* DATE              AUTHOR						NOTE
+* -----------------------------------------------------------
+* 2020.09.18        HUN							최초 생성
+*/
+
 @SpringBootApplication
 public class Test001Application {
 
